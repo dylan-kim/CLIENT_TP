3c1c465216e88a0a4f42587e1504f3be73534054
dylan-kim <dylan.kimd@gmail.com>
2020-06-18 17:48:45 +0200

    Change strings to constants

--- src/com/servlet/ModifierInformationServlet.java
+++ src/com/servlet/ModifierInformationServlet.java
@@ -20,6 +20,16 @@ import com.dto.VilleDTO;
 public class ModifierInformationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	public static final String PARAMETRE_CODE_COMMUNE = "codeCommune";
+	public static final String PARAMETRE_NOM_COMMUNE = "nomCommune";
+	public static final String PARAMETRE_CODE_POSTAL = "codePostal";
+	public static final String PARAMETRE_LIBELLE_ACHEMINEMENT = "libelleAcheminement";
+	public static final String PARAMETRE_LIGNE_5 = "ligne5";
+	public static final String PARAMETRE_LATITUDE = "latitude";
+	public static final String PARAMETRE_LONGITUDE = "longitude";
+	
+	public static final String ATTRIBUT_VILLE = "ville";
+	public static final String SERVLET_AFFICHER_INFORMATION = "afficherInformation";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -33,12 +43,12 @@ public class ModifierInformationServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 
-		String codeCommune = request.getParameter("codeCommune");
+		String codeCommune = request.getParameter(PARAMETRE_CODE_COMMUNE);
 
 		VilleAPI villeAPI = new VilleAPI();
 		VilleDTO ville = villeAPI.trouverVille(codeCommune);
 		
-		session.setAttribute("ville", ville);
+		session.setAttribute(ATTRIBUT_VILLE, ville);
 		RequestDispatcher dispatch = request.getRequestDispatcher("modifierInformation.jsp");
 		dispatch.forward(request, response);
 	}
@@ -47,20 +57,20 @@ public class ModifierInformationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String codeCommune = request.getParameter("codeCommune");
-		String nomCommune = request.getParameter("nomCommune");
-		String codePostal = request.getParameter("codePostal");
-		String libelleAcheminement = request.getParameter("libelleAcheminement");
-		String ligne5 = request.getParameter("ligne5");
-		String latitude = request.getParameter("latitude");
-		String longitude = request.getParameter("longitude");
+		String codeCommune = request.getParameter(PARAMETRE_CODE_COMMUNE);
+		String nomCommune = request.getParameter(PARAMETRE_NOM_COMMUNE);
+		String codePostal = request.getParameter(PARAMETRE_CODE_POSTAL);
+		String libelleAcheminement = request.getParameter(PARAMETRE_LIBELLE_ACHEMINEMENT);
+		String ligne5 = request.getParameter(PARAMETRE_LIGNE_5);
+		String latitude = request.getParameter(PARAMETRE_LATITUDE);
+		String longitude = request.getParameter(PARAMETRE_LONGITUDE);
 
 
 		VilleAPI villeAPI = new VilleAPI();
 		VilleDTO ville = createVille(codeCommune, nomCommune, codePostal, libelleAcheminement, ligne5, latitude, longitude);
 		
 		villeAPI.modifierVille(ville);
-		response.sendRedirect("afficherInformation");
+		response.sendRedirect(SERVLET_AFFICHER_INFORMATION);
 	}
 
 	private VilleDTO createVille(String codeCommune, String nomCommune, String codePostal, String libelleAcheminement, String ligne5,
